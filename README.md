21a2811781ae31474d8e3223869b900e66ddbdcc
Gustavo Hariel <gustavobonja@gmail.com>
2023-03-28 19:08:32 -0300

    app commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # redmine-autoclose
 Script para fechar chamados do Redmine com o Status de Resolvido a mais de 7 dias. é criado um JSON na pasta de logs com os chamados afetados.
+
+# Getting Started
+1.	Definir API KEY do redmine e URL Redmine no index.py.
+2.	Criar Environment utilizando o arquivo requirements.txt
+3.	Rodar index.py
---
+++ index.py
@@ -0,0 +1,58 @@
+from redminelib import Redmine,exceptions
+from datetime import datetime
+from dateutil import parser
+import json
+
+
+# dict para gerar um log JSON
+issue_map = {}
+# url do Redmine
+redmine_url = "baseurl do redmine"
+# api key do redmine
+redmine_key = "sua API key"
+
+def main():
+
+    today = datetime.today().isoformat()
+    date1 = parser.parse(today)
+
+    redmine = Redmine(redmine_url, key=redmine_key)
+
+    # define parametro de busca dos chamados
+    issues = redmine.issue.filter(
+        status_id="3",
+        created_on=">=2017-01-01",
+        sort="updated_on",
+        include="journals"
+    )
+
+    #para cada chamado selecionado, compara a data atual com o ultimo update do chamado.
+    # se diff >= 7, atualiza o status para fechado com a data de fechamento e adiciona uma nota (journal) no chamado.
+    # se sucesso no update, armazena dados e url do chamado para logs.
+
+    for issue in issues:
+        date2 = parser.parse(issue.updated_on.isoformat())
+        diff = date1 - date2
+        if diff.days >= 7:
+            try:
+                issue.update(
+                    1,
+                    notes="Fechado Pela Rotina Automatica Pois Estava No Status Resolvido a Mais de 7 dias",
+                    closed_on=date1,
+                    status_id=5)
+            except exceptions.ValidationError(Exception) :
+                return Exception
+            else:
+                issue_map[issue.id] = (issue,redmine_url + "/issues/" + str(issue.id))
+                print(str(issue.id) + " - Chamado Atualizado")
+
+    # se existe algum chamado atualizado, criar log.
+    if issue_map:
+        with open(f"logs/closed-logs-{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.json", "w") as json_out:
+            json.dump(issue_map, json_out)            
+
+
+    
+if __name__ == "__main__":
+    main()
+    exit("Fim do Script")
